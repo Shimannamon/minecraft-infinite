298eb989b26242f83c52755577d9c536179da15f
Shimanami <151993169+Shimannamon@users.noreply.github.com>
2025-08-17 23:53:14 +0900

    Update csvToTable.js

--- csvToTable.js
+++ csvToTable.js
@@ -1,6 +1,8 @@
 fetch("1.21.8.csv")
   .then((response) => response.text())
   .then((csvText) => {
+    const lines = csvText.trim().split("\n");
+    const dataLines = lines.slice(1);
     const tableBody = document.getElementById("tableBody");
     dataLines.forEach((line) => {
       const [name, id, how, status] = line.split(",");
